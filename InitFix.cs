051a076ea8e0568fc65e16a5840872e60d9dadcf
xill47 <x-i-ll@hotmail.com>
2024-06-05 15:19:01 +0200

    Workaround

---
+++ InitFix.cs
@@ -0,0 +1,11 @@
+﻿using Godot;
+
+namespace GodotDotnetThreadedLoad;
+
+public partial class InitFix : Node
+{
+    public override void _Ready()
+    {
+        ResourceLoader.Load("res://InitFix.cs");
+    }
+}
